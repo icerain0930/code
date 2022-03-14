e2bf44819cb78a5c29d067d1ffcda2136eaa3a55
icerain0930 <62783196+icerain0930@users.noreply.github.com>
2022-03-14 15:35:46 +0800

    Create d804.cpp

---
+++ zerojudge/d804.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool cmp(int a,int b){
+    return a > b;
+}
+int main(){
+int n,m,sum=0,t=0;
+cin>>n>>m;
+int a[n];
+for(int i=0;i<n;i++){
+	cin>>a[i]; 
+}
+sort(a,a+n,cmp);
+
+for(int i=0;i<n;i++){
+	sum+=a[i];
+	t++;
+	if(i==n-1 && sum<m)cout<<"OAQ\n";
+	if(sum>=m){
+	cout<<t<<endl;
+	break;
+	}
+}
+
+return 0;
+}
