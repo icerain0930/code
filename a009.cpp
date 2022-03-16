c70599ff217c23c537ee09168ed288219b568682
icerain0930 <62783196+icerain0930@users.noreply.github.com>
2022-03-16 22:30:30 +0800

    Add files via upload

---
+++ a009.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+//by icerain
+int main(){
+string s;
+cin>>s;
+int len=s.length();
+for(int i=0;i<len;i++){
+	char c=s[i]-12;
+	cout<<c;
+}
+return 0;
+}
+
+
+
+
+
+
+ 
