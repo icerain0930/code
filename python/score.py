34a42ab2bc98321fe01bbb78fe9f7c98b906655b
icerain0930 <62783196+icerain0930@users.noreply.github.com>
2022-10-29 18:10:41 +0800

    Add files via upload

---
+++ python/score.py
@@ -0,0 +1,25 @@
+num = int(input())
+if(num>=95):
+    print("A+")
+elif(90<=num and num<=94):
+    print("A")
+elif(85<=num and num<=89):
+    print("A-")
+elif(80<=num and num<=84):
+    print("B+")
+elif(75<=num and num<=79):
+    print("B")
+elif(70<=num and num<=74):
+    print("B-")
+elif(65<=num and num<=69):
+    print("C+")
+elif(60<=num and num<=64):
+    print("C")
+elif(55<=num and num<=59):
+    print("C-")
+elif(50<=num and num<=54):
+    print("D+")
+elif(45<=num and num<=49):
+    print("D")
+else :
+    print("D-")
