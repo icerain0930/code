a55865db9dacb9c121930617e9b9e5472c96e7c9
icerain0930 <62783196+icerain0930@users.noreply.github.com>
2023-01-23 20:58:10 +0800

    Add files via upload

---
+++ zerojudge/h291.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ios_base::sync_with_stdio(0);
+cin.tie(0); // I/OÀu¤Æ
+string s[12] = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+int a;
+cin>>a;
+cout<<s[a-1];
+return 0;
+}
+
