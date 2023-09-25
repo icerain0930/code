c6a232a29d1599b9ba41a735e6e13f02feb7e6cf
icerain0930 <62783196+icerain0930@users.noreply.github.com>
2023-09-25 20:07:43 +0800

    Add files via upload

--- python/article_title/article.py
+++ python/article_title/article.py
@@ -11,7 +11,15 @@ def choose():
     lab.place(x=0,y=70)
     listday[i]=str(int(listday[i])+1)
 
-def save():
+    #生成js
+    with open("js.txt", "w") as file:
+        file.write("")
+        file.write("document.querySelector(\"#sltKind2\").selectedIndex=2\n") #子分類 > 生活休閒
+        file.write("document.querySelector(\"#title\").value=\"" + text +"\"\n") #標題 
+        file.write("document.querySelector(\"#new_tag\").value=\"日常\"\n") #標籤 > 日常
+        file.write("document.querySelector(\"#All-ages\").click()\n")#閱覽分級 > 全年齡
+        
+def save():#寫入新天數
     strA = ",".join(listday)
     with open("file.txt", "w") as file:
         file.write(strA)
@@ -33,16 +41,19 @@ Var = tk.IntVar()
 #產生radiobutton
 for i in range(len(listtext)+1):
     radio(i)
-
+#生成按鈕
 generate = tk.Button(text="生成",command=choose)
 generate.place(x=0,y=45)
-
+#儲存按鈕
 save = tk.Button(text="儲存",command=save)
 save.place(x=0,y=100)
-
+#讀取天數
 listday = []
 with open("file.txt", "r") as tf:
     lines = tf.read().split(',')
     
 for line in lines:
     listday.append(line)
+
+
+
