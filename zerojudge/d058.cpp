311da85113f346775668170f11a7389aac0658ed
icerain0930 <62783196+icerain0930@users.noreply.github.com>
2023-11-04 00:12:20 +0800

    Create d058.cpp

---
+++ zerojudge/d058.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+//d058
+//by icerain
+int main(){
+ios_base::sync_with_stdio(0);
+cin.tie(0); 
+int n;
+cin>>n;
+n = n>0 ? 1 : n<0 ? -1 : 0 ;
+cout<<n<<endl;
+return 0;
+}
