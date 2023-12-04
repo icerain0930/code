06bb1927655025173a3552322070423fcb05a7b7
icerain0930 <62783196+icerain0930@users.noreply.github.com>
2023-12-04 21:39:03 +0800

    Add files via upload

---
+++ zerojudge/a002.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+//ZJ a002
+//by icerain
+int main(){
+ios_base::sync_with_stdio(0);
+cin.tie(0); // I/OÀu¤Æ
+int a,b;
+cin>>a>>b;
+cout<<a+b<<endl;
+return 0;
+}
+
